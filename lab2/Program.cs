46e477ff0524940b80799b8068919d452d976622
berg1ce <72440034+berg1ce@users.noreply.github.com>
2023-10-05 10:02:04 +0300

    Add files via upload

---
+++ lab2/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+/*
+Разбить каждую строку на отдельные слова. 
+Обрамить каждое слово в рамку из ASCII-символов ('|', '=' и пр.).
+
+*/
+public class Program
+{
+    public static void Main()
+    {
+        int x = 0, y = 0, n = 0, j = 0;
+
+        init(ref x, ref y, ref n);
+        if (checkInput(x, n)) return;
+
+        double[] resul = new double[(int)10 / n + 1];
+        int i = x;
+
+        calc(x, y, n, ref resul, j, i);
+    }
+
+    static void calc(int x, int y, int n, ref double[] resul, int j, int i)
+    {
+        resul[j] = fun(i, y);
+        Console.WriteLine("x = " + i + " \t\t| f(x) = " + resul[j].ToString());
+        j++;
+        i += n;
+
+        if (i > x + 10 )
+        {
+            return;
+        }
+        else
+        {
+            calc(x, y, n, ref resul, j, i);
+        }
+    }
+
+    static double fun(int x, int y)
+    {
+        if (x * y > 0)
+        {
+            return Math.Pow((x * x + y), 2) - Math.Pow((x * x * y), 1 / 2);
+        }
+        else if (x * y < 0)
+        {
+            return Math.Pow((x * x + y), 2) - Math.Pow(Math.Abs(x * x * y), 1 / 2);
+        }
+        else
+        {
+            return Math.Pow((x * x + y), 2) + 1;
+        }
+    }
+    static void init(ref int x, ref int y, ref int n)
+    {
+        Console.WriteLine("Введите значения переменных: \nВведите x: ");
+        Int32.TryParse(Console.ReadLine(), out x);
+        Console.WriteLine("Введите y: ");
+        Int32.TryParse(Console.ReadLine(), out y);
+        Console.WriteLine("Введите шаг n: ");
+        Int32.TryParse(Console.ReadLine(), out n);
+
+    }
+
+
+    static bool checkInput(int x, int n)
+    {
+        if (n <= 0)
+        {
+            Console.WriteLine("Ошибка: шаг n должен быть больше 0");
+            return true;
+        }
+        if (n > Math.Abs(x) + 10)
+        {
+            Console.WriteLine("Ошибка: шаг n должен быть меньше или раным |x|+10");
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        StringBuilder b;
+
+        if (args.Length > 0)
+        {
+            string fileName = @".\" + args[0];
+            string outFile = @".\" + args[1];
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("Ошибка: Файл не существует");
+                return;
+            }
+
+            string[] csFile = File.ReadAllLines(fileName);
+ 
+            b = fun(csFile);
+            File.WriteAllText(@".\" + outFile, b.ToString());
+        }
+        else
+        {
+            string s;
+            int i = 0, n;
+
+            Console.WriteLine("Введите количество строк и сами строки:");
+
+            Int32.TryParse(Console.ReadLine(), out n);
+            string[] arr = new string[n];
+
+            while (i < n)
+            {
+                s = Console.ReadLine();
+                if (s == "") break;
+                arr[i] = s;
+
+                i++;
+            }
+
+            b = fun(arr);
+
+            string res = b.ToString();
+            Console.WriteLine(res);
+        }
+ 
+    }
+
+    static StringBuilder fun(string[] s)
+    {
+
+        StringBuilder b = new StringBuilder();
+
+        foreach (var item in s)
+        {
+            string[] itemArr = item.Split(" ");
+            string[] wordArr = new string[itemArr.Length+1];
+
+            string[] item1 = new string[itemArr.Length+1];
+            for (int i = 0; i < itemArr.Length; i++)
+            {
+                var word = itemArr[i];
+
+                wordArr[i] = "|" + word + "|";
+                item1[i] = new string('=', word.Length + 2);
+            }
+
+            b.AppendJoin(" ",item1);
+            b.Append("\n");
+            b.AppendJoin(" ",wordArr);
+            b.Append("\n");
+            b.AppendJoin(" ",item1);
+            b.Append("\n\n"); 
+        } 
+
+        return b;
+    }
+
+
+
+}
